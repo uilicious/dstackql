8420bafa5682e79ff02edd043f66118a4ce80112
Eugene Cheah <eugeneqin@gmail.com>
2022-08-10 05:43:36 +0000

    lib update, and added support for additional data structures (FileWorkspaceMap, and KeyValueMap)

--- src/main/java/uilicious/dstackql/stack/MultiStackLoader.java
+++ src/main/java/uilicious/dstackql/stack/MultiStackLoader.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Collections;
 import java.util.List;
+import java.util.Set;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.TimeUnit;
@@ -14,6 +15,10 @@ import picoded.core.struct.GenericConvertMap;
 import picoded.dstack.DStack;
 import picoded.dstack.DataObject;
 import picoded.dstack.DataObjectMap;
+import picoded.dstack.FileWorkspace;
+import picoded.dstack.FileWorkspaceMap;
+import picoded.dstack.KeyValue;
+import picoded.dstack.KeyValueMap;
 
 public class MultiStackLoader extends GenericConvertHashMap<String, DStack> {
 	
@@ -168,18 +173,19 @@ public class MultiStackLoader extends GenericConvertHashMap<String, DStack> {
 					long count = 0;
 					
 					// Get the DataObjectMap
-					DataObjectMap dom = stack.getDataObjectMap(structName);
-
+					DataObjectMap sourceDom = stack.getDataObjectMap(structName);
+					
 					// Get the target DOM if relevent
-					DataObjectMap targetDom = (targetStack != null)? targetStack.getDataObjectMap(targetStructName) : null;
-
+					DataObjectMap targetDom = (targetStack != null) ? targetStack
+						.getDataObjectMap(targetStructName) : null;
+					
 					// Trigger any initial table setup for the target if needed
-					if( targetDom != null ) {
+					if (targetDom != null) {
 						targetDom.systemSetup();
 					}
-
+					
 					// Lets iterate it 
-					DataObject obj = dom.looselyIterateObject(null);
+					DataObject obj = sourceDom.looselyIterateObject(null);
 					while (obj != null) {
 						try {
 							if (targetDom != null) {
@@ -195,19 +201,154 @@ public class MultiStackLoader extends GenericConvertHashMap<String, DStack> {
 								obj.keySet();
 							}
 							
-							// Increment and log it
-							count++;
-							if (count % 100 == 1) {
-								System.out.println("[Preloader-" + structType + "] " + stackName + "."
-									+ structName + " : Preloading (" + count + "/" +dom.longSize()+ ") "
-									+ obj._oid());
-							}
-							
 							// lets go to the next object
-							obj = dom.looselyIterateObject(obj);
-							
+							obj = sourceDom.looselyIterateObject(obj);
 						} catch (Exception e) {
 							e.printStackTrace();
+						}
+						
+						// Increment and log it
+						count++;
+						if (count % 100 == 1) {
+							System.out.println("[Preloader-" + structType + "] " + stackName + "."
+								+ structName + " : Preloading (" + count + "/" + sourceDom.longSize()
+								+ ") " + obj._oid());
+						}
+					}
+					
+					// Finish the output
+					System.out.println("[Preloader-" + structType + "] " + stackName + "." + structName
+						+ " : Finished Preloading (" + count + ") !!!");
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			};
+		} else if (structType.equalsIgnoreCase("KeyValueMap")) {
+			return () -> {
+				try {
+					// Log the output
+					System.out.println("[Preloader-" + structType + "] " + stackName + "." + structName
+						+ " : Starting Preloading ...");
+					long count = 0;
+					
+					// Get the DataObjectMap
+					KeyValueMap sourceKVM = stack.getKeyValueMap(structName);
+					
+					// Get the target DOM if relevent
+					KeyValueMap targetKVM = (targetStack != null) ? targetStack
+						.getKeyValueMap(targetStructName) : null;
+					
+					// Trigger any initial table setup for the target if needed
+					if (targetKVM != null) {
+						targetKVM.systemSetup();
+					}
+					
+					// Lets get the full key set list
+					Set<String> keys = sourceKVM.keySet();
+					for (String key : keys) {
+						try {
+							// Lets get the source KV
+							KeyValue kv = sourceKVM.get(key);
+							
+							// The value/expiry
+							String val = null;
+							long exp = -1;
+							
+							// Only process KV with valid values
+							if (kv != null) {
+								val = kv.getValue();
+								exp = kv.getExpiry();
+							}
+							
+							// Sync to target, if its found
+							if (targetKVM != null) {
+								if (val != null) {
+									if (exp > 0) {
+										targetKVM.putWithExpiry(key, val, exp);
+									} else {
+										targetKVM.put(key, val);
+									}
+								}
+							}
+						} catch (Exception e) {
+							e.printStackTrace();
+						}
+						
+						// Increment and log it
+						count++;
+						if (count % 100 == 1) {
+							System.out.println("[Preloader-" + structType + "] " + stackName + "."
+								+ structName + " : Preloading (" + count + "/" + keys.size() + ") " + key);
+						}
+					}
+					
+					// Finish the output
+					System.out.println("[Preloader-" + structType + "] " + stackName + "." + structName
+						+ " : Finished Preloading (" + count + ") !!!");
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			};
+		} else if (structType.equalsIgnoreCase("FileWorkspaceMap")) {
+			return () -> {
+				try {
+					// Log the output
+					System.out.println("[Preloader-" + structType + "] " + stackName + "." + structName
+						+ " : Starting Preloading ...");
+					long count = 0;
+					
+					// Get the DataObjectMap
+					FileWorkspaceMap sourceFWM = stack.getFileWorkspaceMap(structName);
+					
+					// Get the target DOM if relevent
+					FileWorkspaceMap targetFWM = (targetStack != null) ? targetStack
+						.getFileWorkspaceMap(targetStructName) : null;
+					
+					// Trigger any initial table setup for the target if needed
+					if (targetFWM != null) {
+						targetFWM.systemSetup();
+					}
+					
+					// Lets get the full key set list
+					Set<String> keys = sourceFWM.keySet();
+					for (String key : keys) {
+						try {
+							// Lets get the file workspace
+							FileWorkspace sourceFW = sourceFWM.get(key);
+							
+							// Lets get the target file workspace if neeeded
+							FileWorkspace targetFW = (targetFWM != null) ? targetFWM.get(key) : null;
+							
+							// Lets iterate the workspace
+							Set<String> pathList = sourceFW.getFileAndFolderPathSet("");
+							
+							// Lets loop through the path list
+							for (String path : pathList) {
+								// Hanlde file and folder seperately
+								if (path.endsWith("/")) {
+									// Handle folder sync
+									sourceFW.ensureFolderPath(path);
+									if (targetFW != null) {
+										targetFW.ensureFolderPath(path);
+									}
+								} else {
+									// Handle file sync
+									if (targetFW != null) {
+										targetFW.writeByteArray(path, sourceFW.readByteArray(path));
+									} else {
+										sourceFW.readByteArray(path);
+									}
+								}
+							}
+						} catch (Exception e) {
+							e.printStackTrace();
+						}
+						
+						// Increment and log it
+						count++;
+						if (count % 100 == 1) {
+							System.out.println("[Preloader-" + structType + "] " + stackName + "."
+								+ structName + " : Preloading (" + count + "/" + keys.size() + ") " + key);
 						}
 					}
 					
